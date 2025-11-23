04ce5b9d9cf313767cffc20dc16757ca26c5cbd6
TheLazyCowboy1 <Nathan@331oak.com>
2025-11-22 20:56:55 -0500

    Dash fix! finally!

--- Abilities/Dash.cs
+++ Abilities/Dash.cs
@@ -42,47 +42,50 @@ public static class Dash
             info.DashHeld = keyPressed;
 
             //The dash button is being pressed
-            if (info.WantToDash > 0)
+            if (info.WantToDash > 0 && info.DashesLeft > 0 && info.DashCooldown < 1)
             {
-                if (info.DashesLeft < 1 || info.DashCooldown > 0)
-                    return; //don't dash when on cooldown!
+                //if (info.DashesLeft < 1 || info.DashCooldown > 0)
+                    //return; //don't dash when on cooldown!
 
                 //pick dash direction vector
                 Player.InputPackage input = self.input[0];
-                Vector2 dir;
+                Vector2 dir = new(0, 0);
                 if (input.analogueDir.sqrMagnitude > 0.02f)
                     dir = input.analogueDir.normalized;
                 else if (input.x != 0 || input.y != 0)
                     dir = new Vector2(input.x, input.y).normalized; //this shouldn't actually be possible to run; but it's here just in case
-                else
-                    return; //there is no dash direction, so don't use up the dash
+                //else
+                    //return; //there is no dash direction, so don't use up the dash
 
-                dir.y = Mathf.Min(1, dir.y + 0.15f); //add a little extra upwards speed to help counteract gravity
-
-                //actually move the player
-                self.bodyChunks[0].vel = Vector2.LerpUnclamped(self.bodyChunks[0].vel, dir * CurrentAbilities.DashSpeed, CurrentAbilities.DashStrength);
-                self.bodyChunks[1].vel = Vector2.LerpUnclamped(self.bodyChunks[1].vel, dir * CurrentAbilities.DashSpeed, CurrentAbilities.DashStrength * 0.95f); //dash is weaker for tail
-                self.canJump = 0; //don't double-jump!
-                self.wantToJump = 0;
-
-                //sounds
-                self.room.PlaySound(SoundID.Slugcat_Throw_Rock, self.mainBodyChunk, false, 1f, 0.9f);
-
-                //particles
-                for (int i = 0; i < 12; i++)
+                if (dir.x != 0 || dir.y != 0)
                 {
-                    Spark spark = new Spark(self.mainBodyChunk.pos + Custom.RNV() * 10f - self.mainBodyChunk.vel * 3f, self.mainBodyChunk.vel * 0.6f, new(0.8f, 0.8f, 0.9f), null, 15, 20);
-                    spark.gravity *= 0.4f; //half the variation between extremes
-                    spark.gravity += 0.1f; //gravity is in range 0.26 to 0.46
-                    self.room.AddObject(spark);
+                    dir.y = Mathf.Min(1, dir.y + 0.15f); //add a little extra upwards speed to help counteract gravity
+
+                    //actually move the player
+                    self.bodyChunks[0].vel = Vector2.LerpUnclamped(self.bodyChunks[0].vel, dir * CurrentAbilities.DashSpeed, CurrentAbilities.DashStrength);
+                    self.bodyChunks[1].vel = Vector2.LerpUnclamped(self.bodyChunks[1].vel, dir * CurrentAbilities.DashSpeed, CurrentAbilities.DashStrength * 0.95f); //dash is weaker for tail
+                    self.canJump = 0; //don't double-jump!
+                    self.wantToJump = 0;
+
+                    //sounds
+                    self.room.PlaySound(SoundID.Slugcat_Throw_Rock, self.mainBodyChunk, false, 1f, 0.9f);
+
+                    //particles
+                    for (int i = 0; i < 12; i++)
+                    {
+                        Spark spark = new Spark(self.mainBodyChunk.pos + Custom.RNV() * 10f - self.mainBodyChunk.vel * 3f, self.mainBodyChunk.vel * 0.6f, new(0.8f, 0.8f, 0.9f), null, 15, 20);
+                        spark.gravity *= 0.4f; //half the variation between extremes
+                        spark.gravity += 0.1f; //gravity is in range 0.26 to 0.46
+                        self.room.AddObject(spark);
+                    }
+
+                    //info.DashedSincePress = true;
+                    info.DashesLeft--;
+                    info.DashCooldown = Options.DashCooldown;
+                    info.WantToDash = 0;
+
+                    Plugin.Log("Dashed!", 2);
                 }
-
-                //info.DashedSincePress = true;
-                info.DashesLeft--;
-                info.DashCooldown = Options.DashCooldown;
-                info.WantToDash = 0;
-
-                Plugin.Log("Dashed!", 2);
             }
             //else //we're no longer trying to dash, so we can make dashing available again
                 //info.DashedSincePress = false;
