8036b15047f46347d798b2587ab3577ffcd32020
TheLazyCowboy1 <Nathan@331oak.com>
2025-12-11 21:59:11 -0500

    starting work

---
+++ Creatures/Dragon.cs
@@ -0,0 +1,22 @@
+﻿using MetroidvaniaMode.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetroidvaniaMode.Creatures;
+
+public class Dragon : Lizard
+{
+    [EasyExtEnum]
+    public static CreatureTemplate.Type PinkDragon;
+
+    /// <summary>
+    /// Basic constructor, never intended to be used directly
+    /// </summary>
+    public Dragon(AbstractCreature abstractCreature, World world) : base(abstractCreature, world)
+    {
+    }
+
+}
